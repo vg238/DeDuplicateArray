22b05bbd3814ad42d14a6a59123cba40aec5a088
vijayganji <vganji238@gmail.com>
2016-06-01 14:13:55 -0400

    Add files via upload

---
+++ DeDupArray/src/main/java/com/cts/DeDup.java
@@ -0,0 +1,59 @@
+package com.cts;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.primitives.Ints;
+
+public class DeDup {
+	
+	public int[] deDupsUsingArrayList(int[] orgArray){
+		List<Integer> deDupedList = new ArrayList<Integer>();
+		for(int i:orgArray){
+			if(!deDupedList.contains(i))
+			{
+				deDupedList.add(i);
+			}
+		}
+		return Ints.toArray(deDupedList);
+	}
+
+
+	public int[] deDupByLoopingArray(int[] orgArray) {
+		
+		if (orgArray.length < 2)
+			return orgArray;
+		
+		Arrays.sort(orgArray);
+		
+		int j = 0, i = 1;
+		while (i < orgArray.length) {
+			if (orgArray[i] == orgArray[j]) {
+				i++;
+			} else {
+				orgArray[++j] = orgArray[i++];
+			}
+		}
+		int[] resultArray = Arrays.copyOf(orgArray, j + 1);
+	 
+		return resultArray;
+	}
+	
+	// using LinkedHashSet to preserve the ordering
+	public int[] deDupUsingLinkedHashSet(int[] ints){
+	    Set<Integer> set = new LinkedHashSet<Integer>();
+	    for(int i=0;i<ints.length;i++){
+	        set.add(ints[i]);
+	    }
+
+	    return Ints.toArray(set);
+	}
+	
+	public static void main(String[] args){
+		int[] r = {1,2,34,34,25,1,45,3,26};
+		new DeDup().deDupByLoopingArray(r);
+	}
+}
---
+++ DeDupArray/src/test/java/com/cts/DedupTest.java
@@ -0,0 +1,44 @@
+package com.cts;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DedupTest {
+
+	private int[] randomIntegers = {1,2,34,34,25,1,45,3,26,85,4,34,86,25,43,2,1,10000,11,16,19,1,18,4,9,
+									3,20,17,8,15,6,2,5,10,14,12,13,7,8,9,1,2,15,12,18,10,14,20,17,16,3,6,
+								   19,13,5,11,4,7,19,16,5,9,12,3,20,7,15,17,10,6,1,8,18,4,14,13,2,11};
+	private int[] expectedIntegers = {1, 2, 34, 25, 45, 3, 26, 85, 4, 86, 43, 10000, 11, 16, 19, 18, 9,
+																20, 17, 8, 15, 6, 5, 10, 14, 12, 13, 7};
+
+	static DeDup deDup;
+	
+	@BeforeClass
+	public static void initialize(){
+		deDup = new DeDup();
+	}
+	
+	@Test
+	public void test_deDupsUsingArrayList() {
+		assertArrayEquals(expectedIntegers, deDup.deDupsUsingArrayList(randomIntegers));		
+	}
+	
+	@Test
+	public void test_deDupUsingLinkedHashSet() {
+		assertArrayEquals(expectedIntegers, deDup.deDupUsingLinkedHashSet(randomIntegers));	
+	}
+	
+	//Result from deDup.removeDuplicates(randomIntegers) is sorted & deduplicated array. To compare the arrays, we are sorting extectedIntergers first.
+	@Test
+	public void test_deDupByLoopingArray() {
+		Arrays.sort(expectedIntegers);
+		assertArrayEquals(expectedIntegers,deDup.deDupByLoopingArray(randomIntegers));
+		
+		
+	}
+
+}
